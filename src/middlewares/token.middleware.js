0b172540b53e38d01ad6a0ae4d70b99416a94a36
superbeom <it.super.beom@gmail.com>
2022-12-19 13:39:38 +0900

    Create Token Middleware

---
+++ src/middlewares/token.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import responseHandler from "../handlers/response.handler.js";
+import userModel from "../models/user.model.js";
+
+const tokenDecode = (req) => {
+  try {
+    const bearerHeader = req.headers["authorization"];
+
+    if (bearerHeader) {
+      const token = bearerHeader.split(" ")[1];
+
+      return jwt.verify(token, process.env.SECRET_KEY);
+    }
+
+    return false;
+  } catch {
+    return false;
+  }
+};
+
+const auth = async (req, res, next) => {
+  const tokenDecoded = tokenDecode(req);
+
+  if (!tokenDecoded) return responseHandler.unauthorize(res);
+
+  const user = await userModel.findById(tokenDecoded.data);
+
+  if (!user) return responseHandler.unauthorize(res);
+
+  req.user = user;
+
+  next();
+};
+
+export default { auth, tokenDecode };
