88c0857df565d731136efc8c83adc5fc6f5e5127
superbeom <it.super.beom@gmail.com>
2022-12-19 12:41:40 +0900

    Create Models

---
+++ src/models/review.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+import modelOptions from "./model.options.js";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    mediaType: {
+      type: String,
+      enum: ["tv", "movie"],
+      required: true,
+    },
+    mediaId: {
+      type: String,
+      required: true,
+    },
+    mediaTitle: {
+      type: String,
+      required: true,
+    },
+    mediaPoster: {
+      type: String,
+      required: true,
+    },
+  },
+  modelOptions
+);
+
+const reviewModel = mongoose.model("Review", reviewSchema);
+
+export default reviewModel;
