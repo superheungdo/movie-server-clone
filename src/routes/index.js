1ce2b7997a04216139ae6e40b3b124d95e94f17b
superbeom <it.super.beom@gmail.com>
2022-12-19 14:03:44 +0900

    Create Person Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRoute from "./user.route.js";
 import mediaRoute from "./media.route.js";
+import personRoute from "./person.route.js";
 
 const router = Router();
 
 router.use("/user", userRoute);
 router.use("/:mediaType", mediaRoute);
+router.use("/person", personRoute);
 
 export default router;
