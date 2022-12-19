37d76ea51e830d2e7017575981fd300c23fad7e4
superbeom <it.super.beom@gmail.com>
2022-12-19 14:02:59 +0900

    Create Media Route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoute from "./user.route.js";
+import mediaRoute from "./media.route.js";
 
 const router = Router();
 
 router.use("/user", userRoute);
+router.use("/:mediaType", mediaRoute);
 
 export default router;
